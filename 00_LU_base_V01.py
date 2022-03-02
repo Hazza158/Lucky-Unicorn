cabc04d0b67d7c70c4c2b54af16b7491b1fb8cc1
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2022-03-03 12:25:53 +1300

    Add files via upload

--- 00_LU_base_V01.py
+++ 00_LU_base_V01.py
@@ -1,20 +1,20 @@
+# checks for yes / no, returns "yes" / "no"
 def yes_no(question):
     valid = False
     while not valid:
         response = input(question).lower()
         
         if response == "yes" or response == "y":
-            response = "yes"
-            return response
+            return "yes"
             
         
         elif response == "no" or response == "n":
-            response = "no"
-            return response
+            return "no"
         
         else:
             print("please answer yes / no")
-        
+
+# Displays instructions, returns ""        
 def instructions():
     print("**** How to Play ****")
     print()
@@ -22,10 +22,35 @@ def instructions():
     print()
     return "" 
 
-played_before = yes_no("have you played the game before?")
 
+def num_check(question, low, high) :
+    error = "please enter a whole number between 1 and 10\n"
+
+    valid = False 
+    while not valid:
+        try:
+            response = int(input(question)) 
+
+            if low < response <= high:
+                return response
+
+
+            else:
+                print(error)
+
+        except ValueError:
+                print(error)
+
+
+# ***** Main Routine goes here ****
+
+played_before = yes_no("have you played the game before?")
 
 if played_before == "no": 
     instructions()
-    
-print("program continues")
+
+
+how_much = num_check("how much would you like to play with? ", 0, 10)
+
+print("You are playing with ${:.2f}".format(how_much))
+
